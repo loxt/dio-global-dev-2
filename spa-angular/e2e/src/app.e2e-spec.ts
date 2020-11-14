72efa60a1eb230020e93af075cf79bd9e95888c5
loxt <emannuelloxt@hotmail.com>
2020-11-13 23:01:43 -0400

    refactor: project setup

--- spa-angular/e2e/src/app.e2e-spec.ts
+++ spa-angular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('spa-angular app is running!');
+    expect(page.getTitleText()).toEqual('spaangular app is running!');
   });
 
   afterEach(async () => {
--- spa-angular/src/app/app.component.ts
+++ spa-angular/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'spa-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spa-angular';
+  title = 'spaangular';
 }
